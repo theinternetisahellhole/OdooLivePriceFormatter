a077f25871c458a153daa1c73fc3e3e6ab3f4040
THE INTERNET IS A HELLHOLE <fouadsalehiadd@gmail.com>
2026-06-23 20:02:26 +0330

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Sale Unit Price Format",
+    "version": "18.0.1.0.0",
+    "category": "Sales",
+    "depends": ["sale"],
+    "assets": {
+        "web.assets_backend": [
+            "sale_unit_price_format/static/src/js/unit_price_format.js",
+        ],
+    },
+    "installable": True,
+    "application": False,
+}
